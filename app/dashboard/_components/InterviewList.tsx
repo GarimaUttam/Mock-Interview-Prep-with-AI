0a606a0a9a76a4dd670f54fd43aaf6b0e2ef1b28
GarimaUttam <garimauttam191203@gmail.com>
2024-06-19 13:44:54 +0530

    minor CSS fix

--- app/dashboard/_components/InterviewList.tsx
+++ app/dashboard/_components/InterviewList.tsx
@@ -1,18 +1,22 @@
 "use client";
+
 import { db } from "@/utils/db";
 import { MockInterview } from "@/utils/schema";
 import { useUser } from "@clerk/nextjs";
 import { desc, eq } from "drizzle-orm";
 import React, { useEffect, useState } from "react";
-import InterviewItemCard from "./InterviewItemCard"
+import InterviewItemCard from "./InterviewItemCard";
 
 const InterviewList = () => {
   const { user } = useUser();
-  const [InterviewList, setInterviewList] = useState([]);
+
+  const [interviewList, setInterviewList] = useState([]);
+
   useEffect(() => {
-    user && GetInterviewList();
+    user && getInterviewList();
   }, [user]);
-  const GetInterviewList = async () => {
+
+  const getInterviewList = async () => {
     const result = await db
       .select()
       .from(MockInterview)
@@ -21,18 +25,15 @@ const InterviewList = () => {
       )
       .orderBy(desc(MockInterview.id));
 
-    console.log(
-      "🚀 ~ file: InterviewList.jsx:14 ~ GetInterviewList ~ GetInterviewList:",
-      GetInterviewList
-    );
-    setInterviewList(result)
+    setInterviewList(result);
   };
+
   return (
     <div>
-      <h2 className="font-medium text-xl">Previous Mock Interview</h2>
+      <h2 className="font-medium text-xl text-blue-700">Previous Mock Interviews</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-3">
-        {InterviewList&&InterviewList.map((interview,index)=>(
-            <InterviewItemCard interview={interview} key={index}/>
+        {interviewList && interviewList.map((interview, index) => (
+            <InterviewItemCard key={index} interview={interview}/>
         ))}
       </div>
     </div>
